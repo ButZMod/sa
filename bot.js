b3ab344c7f2beaf70e593cbb66f61da36677c265
But'ZMod <77898733+ButZMod@users.noreply.github.com>
2022-01-25 02:46:31 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,16 @@
+const ByErdemcakiroglu = require('./tokenler/token1.json')
+const Discord = require('discord.js')
+//----------------------------------Token 1----------------------------// 
+for (const token of ByErdemcakiroglu.Token1) {
+    const client = new Discord.Client()
+    client.on('ready', () => {
+        console.log(`Token | ${client.user.tag} - ${client.user.id}`)
+    })
+
+  client.on("ready", async () => {
+	client.channels.get("ses-kanalid").join()
+  });
+
+    client.login(token)
+}
+//----------------------------------Token 1 SON----------------------------// 
